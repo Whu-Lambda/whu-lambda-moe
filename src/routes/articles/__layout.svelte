d9327b95139f2fbc40ed9f7f1a59887c2521985e
TlMegalovania <tlmegalovania@qq.com>
2022-01-18 12:49:58 +0800

    refactor: remove auto adapter change articles & events to undefined

--- src/routes/articles/__layout.svelte
+++ src/routes/articles/__layout.svelte
@@ -12,59 +12,59 @@
     <h4>Categories</h4>
     <div class="hline" />
     <p>
-      <a href=""><i class="fa fa-angle-right" /> Report</a>
+      <a href="/"><i class="fa fa-angle-right" /> Report</a>
       <span class="badge badge-theme pull-right">15</span>
     </p>
     <p>
-      <a href=""><i class="fa fa-angle-right" /> Fedora</a>
+      <a href="/"><i class="fa fa-angle-right" /> Fedora</a>
       <span class="badge badge-theme pull-right">1</span>
     </p>
     <p>
-      <a href=""><i class="fa fa-angle-right" /> Server</a>
+      <a href="/"><i class="fa fa-angle-right" /> Server</a>
       <span class="badge badge-theme pull-right">1</span>
     </p>
     <p>
-      <a href=""><i class="fa fa-angle-right" /> Arrangement</a>
+      <a href="/"><i class="fa fa-angle-right" /> Arrangement</a>
       <span class="badge badge-theme pull-right">2</span>
     </p>
     <p>
-      <a href=""><i class="fa fa-angle-right" /> Ospp</a>
+      <a href="/"><i class="fa fa-angle-right" /> Ospp</a>
       <span class="badge badge-theme pull-right">3</span>
     </p>
     <p>
-      <a href=""><i class="fa fa-angle-right" /> Open source</a>
+      <a href="/"><i class="fa fa-angle-right" /> Open source</a>
       <span class="badge badge-theme pull-right">3</span>
     </p>
     <p>
-      <a href=""><i class="fa fa-angle-right" /> Community</a>
+      <a href="/"><i class="fa fa-angle-right" /> Community</a>
       <span class="badge badge-theme pull-right">3</span>
     </p>
     <p>
-      <a href=""><i class="fa fa-angle-right" /> Award</a>
+      <a href="/"><i class="fa fa-angle-right" /> Award</a>
       <span class="badge badge-theme pull-right">1</span>
     </p>
     <p>
-      <a href=""><i class="fa fa-angle-right" /> Duanzi</a>
+      <a href="/"><i class="fa fa-angle-right" /> Duanzi</a>
       <span class="badge badge-theme pull-right">1</span>
     </p>
     <p>
-      <a href=""><i class="fa fa-angle-right" /> Debian</a>
+      <a href="/"><i class="fa fa-angle-right" /> Debian</a>
       <span class="badge badge-theme pull-right">1</span>
     </p>
     <div class="spacing" />
     <h4>Popular Tags</h4>
     <div class="hline" />
     <p>
-      <a class="btn btn-theme" href="" role="button">Report (15)</a>
-      <a class="btn btn-theme" href="" role="button">Fedora (1)</a>
-      <a class="btn btn-theme" href="" role="button">Server (1)</a>
-      <a class="btn btn-theme" href="" role="button">Arrangement (2)</a>
-      <a class="btn btn-theme" href="" role="button">Ospp (3)</a>
-      <a class="btn btn-theme" href="" role="button">Open source (3)</a>
-      <a class="btn btn-theme" href="" role="button">Community (3)</a>
-      <a class="btn btn-theme" href="" role="button">Award (1)</a>
-      <a class="btn btn-theme" href="" role="button">Duanzi (1)</a>
-      <a class="btn btn-theme" href="" role="button">Debian (1)</a>
+      <a class="btn btn-theme" href="/" role="button">Report (15)</a>
+      <a class="btn btn-theme" href="/" role="button">Fedora (1)</a>
+      <a class="btn btn-theme" href="/" role="button">Server (1)</a>
+      <a class="btn btn-theme" href="/" role="button">Arrangement (2)</a>
+      <a class="btn btn-theme" href="/" role="button">Ospp (3)</a>
+      <a class="btn btn-theme" href="/" role="button">Open source (3)</a>
+      <a class="btn btn-theme" href="/" role="button">Community (3)</a>
+      <a class="btn btn-theme" href="/" role="button">Award (1)</a>
+      <a class="btn btn-theme" href="/" role="button">Duanzi (1)</a>
+      <a class="btn btn-theme" href="/" role="button">Debian (1)</a>
     </p>
   </div>
 </div>
